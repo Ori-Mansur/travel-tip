9a3029b55c67e81addeea058ba34a3ff307a39b8
Ori Mansur <orimansur@gmail.com>
2019-10-30 18:10:36 +0200

    travel tip 009

--- js/main.js
+++ js/main.js
@@ -4,8 +4,7 @@ import locService from './services/loc.service.js'
 import mapService from './services/map.service.js'
 
 
-locService.getLocs()
-    .then(locs => console.log('locs', locs))
+
 
 
 
@@ -13,20 +12,20 @@ window.onload = () => {
     mapService.initMap()
         .then(() => {
             mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
+            hendelPos()
+
         })
         .catch(console.log('INIT MAP ERROR'));
 
 
-    locService.getPosition()
-        .then(pos => {
-            console.log('User position is:', pos.coords);
-        })
-        .catch(err => {
-            console.log('err!!!', err);
-        })
+
 }
 
 document.querySelector('.btn').addEventListener('click', (ev) => {
+    hendelPos()
+})
+
+function hendelPos() {
     locService.getPosition()
         .then(pos => {
             console.log('User position is:', pos.coords);
@@ -35,17 +34,19 @@ document.querySelector('.btn').addEventListener('click', (ev) => {
                     mapService.addMarker({ lat: pos.coords.latitude, lng: pos.coords.longitude })
                     mapService.getAddressName(pos.coords.latitude, pos.coords.longitude)
                         .then(name => {
-                            console.log(name.results[0].formatted_address);
+                            renderAddress(name.results[0].formatted_address);
 
                         })
                 })
         })
-    console.log('Aha!', ev.target);
-})
+}
 
 
 function renderAddress(address) {
-    document.querySelector('h3').innerText = address;
+    document.querySelector('h3').innerText = 'Location: ' + address;
+    document.querySelector('.address-input').placeholder=address
+    
+    
 }
 document.querySelector('.address-input').addEventListener('input', (ev) => {
 
--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,20 +1,7 @@
 export default {
-    getLocs: getLocs,
     getPosition: getPosition
 }
 
-
-
-var locs = [{ lat: 11.22, lng: 22.11 }]
-
-function getLocs() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(locs);
-        }, 2000)
-    });
-
-}
 
 
 function getPosition() {
@@ -22,7 +9,7 @@ function getPosition() {
 
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject)
-        
+
     })
 }
 
