1b9feb4d4c02e28c81347b46e89dc3bc95afc7be
Ori Mansur <orimansur@gmail.com>
2019-10-30 15:10:23 +0200

    travel tip 006

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,4 +1,15 @@
+
+export default {
+    getLocs :getLocs,
+    getPosition: getPosition
+}
+
+
+
 var locs = [{lat: 11.22, lng: 22.11}]
+
+
+
 
 function getLocs() {
     return new Promise((resolve, reject)=>{
@@ -18,9 +29,16 @@ function getPosition() {
     })
 }
 
+getLocsobj()
+function getLocsobj(){
+    var prm=axios.get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBtIZbV3hkdA38vvKGEGbrpEah3vO1ZPyE')
+    var prm1=prm.then(res=>res.data)
+    .then(loc=>{
 
-
-export default {
-    getLocs :getLocs,
-    getPosition: getPosition
+        console.log(loc);
+    })
+    
 }
+
+
+
