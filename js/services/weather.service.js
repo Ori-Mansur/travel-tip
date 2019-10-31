9a81ba828430c298ebad887f31ee3dea55a60852
Ori Mansur <orimansur@gmail.com>
2019-10-31 09:37:38 +0200

    travel tip 013

--- js/services/weather.service.js
+++ js/services/weather.service.js
@@ -7,7 +7,7 @@ export default {
 
 
 function getWeather(lat, lon) {
-    return axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=1960f555fc16289521b949e9f716c0fa`)
+    return axios.get(`//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=1960f555fc16289521b949e9f716c0fa`)
         .then(res => res.data)
 
 }
